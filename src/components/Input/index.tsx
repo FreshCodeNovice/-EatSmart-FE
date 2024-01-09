9b0e78d97e2fb076e2c0f6b593f1724cfcc1fe73
Kim0426 <706shin1728@naver.com>
2024-01-09 16:11:07 +0900

    feat: 기본적인 입력 컴포넌트 추가

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,61 @@
+import { forwardRef, InputHTMLAttributes, useRef, FocusEvent } from 'react';
+import styled from 'styled-components';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactNode;
+}
+
+const BaseInput = styled.input`
+  background-color: #f5f5f5;
+  border-radius: 3rem;
+  padding-left: 2.5rem;
+  width: 14.375rem;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  height: auto;
+`;
+
+const IconWrapper = styled.i`
+  position: absolute;
+  left: 1rem;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 1rem;
+  height: 1rem;
+  overflow: hidden;
+`;
+
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    if (props.onBlur) {
+      props.onBlur(e);
+    }
+  };
+
+  return (
+    <Wrapper>
+      {props.icon && <IconWrapper>{props.icon}</IconWrapper>}
+      <BaseInput
+        {...props}
+        ref={inputRef}
+        onClick={handleClick}
+        onBlur={handleBlur}
+      />
+    </Wrapper>
+  );
+});
+
+export default Input;
+
+Input.displayName = 'Input';
