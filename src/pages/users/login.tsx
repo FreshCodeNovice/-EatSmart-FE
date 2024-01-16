54d77058d5af407a6e8fdb4191d3497326b3bce9
sinamong0620 <sinaskim60@gmail.com>
2024-01-17 01:15:48 +0900

    feat: kakaologin feature 추가

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,6 +1,12 @@
-import styled from 'styled-components';
-
 const login = () => {
-    return <>아니 이거 맞음?</>
-}
+  return (
+    <>
+      <button>
+        <a href={process.env.REACT_APP_KAKAO_AUTH_URL}>
+          <div>카카오 아이디로 로그인</div>
+        </a>
+      </button>
+    </>
+  );
+};
 export default login;
