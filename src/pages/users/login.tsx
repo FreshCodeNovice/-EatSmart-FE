791b75c48dff027917624f617d91e2e21bd9b62a
Kim0426 <706shin1728@naver.com>
2024-01-25 00:34:29 +0900

    refactor: kakao login.tsx파일 추가

---
+++ src/pages/users/login.tsx
@@ -0,0 +1,12 @@
+const login = () => {
+  return (
+    <>
+      <button>
+        <a href={`${process.env.REACT_APP_KAKAO_AUTH_URL}`}>
+          <div>카카오 아이디로 로그인</div>
+        </a>
+      </button>
+    </>
+  );
+};
+export default login;
